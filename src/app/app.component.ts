c24baeca64f4767a66b2afb2d29e617d5494069b
Bryan Mamani <gxbryanmc@gmail.com>
2020-11-10 21:33:25 -0500

    Aplicación base del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'proyecto-angular';
+export class AppComponent implements OnInit {
+  title = 'Proyecto Final';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
 }
